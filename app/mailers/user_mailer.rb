d47f110d063e55b5e465e6961a875bccf6b71288
Ryan Itzcovitz <ryanitzcovitz@gmail.com>
2023-06-07 18:33:51 -0300

    Al crear usuario se envía mail, y al crear proyecto se le envía mail al gerente y al líder.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,38 @@
+class UserMailer < ApplicationMailer
+  def welcome_email(usuario)
+    @usuario = usuario
+    mail(to: @usuario.email, subject: "¡Bienvenido a nuestro sitio web!")
+
+    # Crear notificación para el usuario
+    Notificacion.create(usuario_id: @usuario.id, notificacion_tipo: 'Bienvenida', mensaje: '¡Bienvenido a nuestro sitio web!', fecha_hora: DateTime.now)
+  end
+
+  def project_created_email(user, project)
+    @user = user
+    @project = project
+    mail(to: @user.email, subject: "Proyecto creado exitosamente")
+  end
+
+  def project_assigned_email(user, project)
+    @user = user
+    @project = project
+    mail(to: @user.email, subject: "Proyecto asignado a ti")
+
+  end
+
+  def meta_created_email(user, meta)
+    @user = user
+    @meta = meta
+    mail(to: @user.email, subject: "Meta creada exitosamente")
+
+    # Crear notificación para el usuario
+    Notificacion.create(usuario_id: @user.id, notificacion_tipo: 'Meta', mensaje: 'Has creado una nueva meta', fecha_hora: DateTime.now)
+  end
+
+  def tarea_created_email(lider, integrante, tarea)
+    @lider = lider
+    @integrante = integrante
+    @tarea = tarea
+    mail(to: [@lider.email, @integrante.email], subject: "Tarea creada exitosamente")
+  end
+end
